bc26fc62c7aacb0851e0b3aa33a885af85cc3b43
leonardossev <leonardossev@gmail.com>
2020-09-23 05:52:54 -0300

    feat(filter): set http exception filter globally

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './shared/filter/http-exception.filter';
 import { HttpLoggingInterceptor } from './shared/interceptor/http-logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new HttpLoggingInterceptor());
 
   await app.listen(3000);
